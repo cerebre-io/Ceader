ee6565d41878aa65a4a1f98dbecea9507d15b08b
KamilJankowski <kamil@cerebre.io>
2022-04-26 11:03:13 +0200

    setup update

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="ceader",
     version="0.0.1",
@@ -12,6 +15,13 @@ setup(
     python_requiers=">=3.8",
     py_modules=["ceader"],  # Name of the python package
     package_dir={"": "ceader"},
+    description="Tool for automatically adding a header to files in the form of a comment.",
+    long_description=long_description,
+    entry_points={
+        "console_scripts": [
+            "ceader=ceader.__main__:main",
+        ]
+    },
 )
 
 # python setup.py sdist
