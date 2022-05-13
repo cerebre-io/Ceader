17c1de059c86754601402e33cd517a7399a45fcd
Kamil Jankowski <mr.kamil.jankowski@gmail.com>
2022-05-13 22:57:33 +0200

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="ceader",
-    version="0.0.7",
+    version="0.0.8",
     license="MIT",
     author="Kamil Jankowski",
     author_email="mr.kamil.jankowski@gmail.com",
